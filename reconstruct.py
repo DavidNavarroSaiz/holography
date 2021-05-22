fb3fe4311f89435d4c537371aaa6776db2ab2e06
David C Navarro <32474368+DavidNavarroSaiz@users.noreply.github.com>
2021-05-21 21:30:58 -0500

    readme_result

--- reconstruct.py
+++ reconstruct.py
@@ -9,37 +9,42 @@ from holography_tools import HolographyTools
 tools = HolographyTools()
 plt.figure(figsize=(20,20))
 # filename='/content/holoMHDL512.bmp'
-filename='./resources/G4_fly.bmp'
+filename='./resources/holoMHDL512.bmp'
 # filename='/content/Paramecium_Holo.bmp'
 holo=(plt.imread(filename)).astype(float)
 shape=np.shape(holo)
 fi = shape[0]
 # filenameref='/content/refMHDL512.bmp'
-filenameref='./resources/GREF4_fly.bmp'
+filenameref='./resources/refMHDL512.bmp'
 # filenameref='/content/Paramecium_Reference.bmp'
 ref=(plt.imread(filenameref)).astype(float)
 holoContrast=holo - ref
 
-plt.subplot(251)
-plt.title('Holo')
-plt.imshow(holo,cmap='gray')
-plt.subplot(252)
-plt.title('reference')
-plt.imshow(ref,cmap='gray')
-plt.subplot(253)
+# plt.subplot(251)
+# plt.title('Holo')
+# plt.imshow(holo,cmap='gray')
+# plt.subplot(252)
+# plt.title('reference')
+# plt.imshow(ref,cmap='gray')
+plt.subplot(121)
 plt.title('holoContrast')
 plt.imshow(holoContrast,cmap='gray')
-
+# cv2.imshow("initial hologram",holo)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
 #geometric features
-z=2.6e-03
-L=12e-03
-lambda_=650e-09
-dx=(0.006875) / fi
+z=76e-6
+L=12e-2
+lambda_= 405e-9
+dx=(12.288e-2) / fi
+# z=5e-03
+# L=12e-03
+# lambda_=650e-09
+# dx=(0.006875) / fi
 #pixel size at reconstruction plance
 deltaX=(z*dx) / L
   
 #cosenus filter creation
-print(fi)
 FC=tools.filtcosenoF(100,fi)
 #reconstrcut
 
@@ -52,16 +57,16 @@ amplitude = np.abs(K)
 intensity = np.square(np.abs(K))
 phase = np.angle(K)
 
-plt.subplot(254)
-plt.title('Amplitude')
-plt.imshow(amplitude,cmap='gray')
+# plt.subplot(254)
+# plt.title('Amplitude')
+# plt.imshow(amplitude,cmap='gray')
 
-plt.subplot(255)
+plt.subplot(122)
 plt.title('intensity')
 plt.imshow(intensity,cmap='gray')
 
-plt.subplot(256)
-plt.title('Phase')
-plt.imshow(phase,cmap='gray')
+# plt.subplot(256)
+# plt.title('Phase')
+# plt.imshow(phase,cmap='gray')
 
 plt.show()
